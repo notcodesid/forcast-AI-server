e2e95a913656660bf5adc9572cb75b9fcaae27dc
siddharth <notcodesid@gmail.com>
2025-02-18 11:34:27 +0530

    update code

--- index.js
+++ index.js
@@ -35,12 +35,18 @@ const openai = new OpenAI({
 });
 
 // Initialize Google Sheets Authentication
-const credentials = JSON.parse(readFileSync('./google-credentials.json'));
-const serviceAccountAuth = new JWT({
-    email: credentials.client_email,
-    key: credentials.private_key,
-    scopes: ['https://www.googleapis.com/auth/spreadsheets'],
-});
+const serviceAccount = {
+  type: "service_account",
+  project_id: process.env.GOOGLE_PROJECT_ID,
+  private_key_id: process.env.GOOGLE_PRIVATE_KEY_ID,
+  private_key: process.env.GOOGLE_PRIVATE_KEY,
+  client_email: process.env.GOOGLE_CLIENT_EMAIL,
+  client_id: process.env.GOOGLE_CLIENT_ID,
+  auth_uri: process.env.GOOGLE_AUTH_URI,
+  token_uri: process.env.GOOGLE_TOKEN_URI,
+  auth_provider_x509_cert_url: process.env.GOOGLE_AUTH_PROVIDER_X509_CERT_URL,
+  client_x509_cert_url: process.env.GOOGLE_CLIENT_X509_CERT_URL
+};
 
 async function getSheetData(spreadsheetId) {
     const doc = new GoogleSpreadsheet(spreadsheetId, serviceAccountAuth);
